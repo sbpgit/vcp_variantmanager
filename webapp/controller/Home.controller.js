accd7ed1e4c116abfd1dc3e8b7c5f46ebfd27c1a
Pradeep Daka <pradeepkumardaka@sbpcorp.in>
2025-10-08 07:19:56 +0000

    New changes regarding deployment

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "sap/m/MessageToast"
-], (Controller, JSONModel, MessageToast) => {
+    "sap/m/MessageToast",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], (Controller, JSONModel, MessageToast,Filter,FilterOperator) => {
     "use strict";
     var that;
     return Controller.extend("vcpapp.vcpvariantmanagement.controller.Home", {
@@ -28,35 +30,55 @@ sap.ui.define([
         onDeletePress: function () {
             sap.ui.core.BusyIndicator.show();
             var selectedID = that.byId("idVariantTabVM").getSelectedItems();
-            var idObject={}, idArray=[];
-            selectedID.forEach(id=>{
-                idObject.ID= parseInt(id.getCells()[3].getText());
+            var idObject = {}, idArray = [];
+            selectedID.forEach(id => {
+                idObject.ID = parseInt(id.getCells()[3].getText());
                 idArray.push(idObject);
-                idObject={};
+                idObject = {};
             });
-            if(idArray.length>0){
+            if (idArray.length > 0) {
                 this.getOwnerComponent().getModel("oModel").callFunction("/createVariant", {
-                method: "GET",
-                urlParameters: {
-                    Flag : "D", 
-                    USER : "", 
-                    VARDATA : JSON.stringify(idArray)
-                },
-                success: (oData) => {
+                    method: "GET",
+                    urlParameters: {
+                        Flag: "D",
+                        USER: "",
+                        VARDATA: JSON.stringify(idArray)
+                    },
+                    success: (oData) => {
                         MessageToast.show("Deleted successfully");
                         this.onAfterRendering();
-                    sap.ui.core.BusyIndicator.hide();
-                },
-                error: () => {
-                    sap.ui.core.BusyIndicator.hide();
-                    sap.m.MessageToast.show("Error while deleting");
-                }
-            });
+                        sap.ui.core.BusyIndicator.hide();
+                    },
+                    error: () => {
+                        sap.ui.core.BusyIndicator.hide();
+                        sap.m.MessageToast.show("Error while deleting");
+                    }
+                });
             }
-            else{
-                sap.ui.core.BusyIndicator.show();
+            else {
+                sap.ui.core.BusyIndicator.hide();
                 MessageToast.show("No items selected for deletion");
             }
+        },
+        handleSearch: function (oEvent) {
+            var sQuery =
+                oEvent.getParameter("value") || oEvent.getParameter("newValue"),
+                sId = oEvent.getParameter("id"),
+                oFilters = [];
+            // Check if search filter is to be applied
+            sQuery = sQuery ? sQuery.trim() : "";
+            if (sQuery !== "") {
+                oFilters.push(
+                    new Filter({
+                        filters: [
+                            new Filter("USER", FilterOperator.Contains, sQuery),
+                            new Filter("APPLICATION_NAME", FilterOperator.Contains, sQuery)
+                        ],
+                        and: false,
+                    })
+                );
+            }
+            that.byId("idVariantTabVM").getBinding("items").filter(oFilters);
         }
     });
 });
